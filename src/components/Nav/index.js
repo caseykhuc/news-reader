68759e5c9f7a53bade6cfc0d1eac05d67e2fbe55
trangkhuc <khuctrang1812@gmail.com>
2019-11-13 15:32:48 +0700

    minor change to nav toggles

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,20 +6,27 @@ import { Title, Icon, NavSection, Container } from "./styles";
 const Nav = ({ layout, theme, setLayout, setTheme }) => {
   return (
     <Container>
-      <Title>News Reader</Title>
+      <Title>Hacker News Reader</Title>
       <NavSection>
-        <Icon onClick={() => setTheme("dark")}>
-          <i className="fas fa-moon" />
-        </Icon>
-        <Icon onClick={() => setTheme("light")}>
-          <i className="fas fa-sun" />
-        </Icon>
-        <Icon onClick={() => setLayout("list")}>
-          <i className="fas fa-th-list" />
-        </Icon>
-        <Icon onClick={() => setLayout("grid")}>
-          <i className="fas fa-th-large" />
-        </Icon>
+        {theme === "dark" ? (
+          <Icon onClick={() => setTheme("light")}>
+            <i className="fas fa-sun" />
+          </Icon>
+        ) : (
+          <Icon onClick={() => setTheme("dark")}>
+            <i className="fas fa-moon" />
+          </Icon>
+        )}
+
+        {layout === "grid" ? (
+          <Icon onClick={() => setLayout("list")}>
+            <i className="fas fa-th-list" />
+          </Icon>
+        ) : (
+          <Icon onClick={() => setLayout("grid")}>
+            <i className="fas fa-th-large" />
+          </Icon>
+        )}
       </NavSection>
     </Container>
   );
